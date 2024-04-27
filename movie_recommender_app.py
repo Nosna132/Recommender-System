370dc3172b8b65526a04f5606aa44dcbc4369154
Nosna132 <141562783+Nosna132@users.noreply.github.com>
2024-04-27 23:11:51 +0800

    Update movie_recommender_app.py

--- movie_recommender_app.py
+++ movie_recommender_app.py
@@ -5,6 +5,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 import difflib as df
 from joblib import load
 
+# Load data
+tmdb_data = pd.read_csv('tmdb_5000_movies.csv')
+
 # Function to handle errors and variations in user input
 def find_closest_match(user_input, movie_titles):
     closest_matches = df.get_close_matches(user_input, movie_titles, n=1, cutoff=0.6)
@@ -36,17 +39,6 @@ def content_based_filtering(movie_title, model, tmdb_data):
 def main():
     st.title("Movie Recommender System")
 
-    # Load data
-    try:
-        tmdb_data = pd.read_csv('tmdb_5000_movies.csv')
-        movie_titles = tmdb_data['title'].tolist()
-    except FileNotFoundError:
-        st.error("File 'tmdb_5000_movies.csv' not found.")
-        return
-    except pd.errors.EmptyDataError:
-        st.error("File 'tmdb_5000_movies.csv' is empty.")
-        return
-
     # User input for movie title
     movie_title = st.text_input("Enter the title of the movie:")
 
@@ -54,15 +46,19 @@ def main():
     filtering_method = st.selectbox("Select Filtering Method:", ["Collaborative Filtering", "Content-Based Filtering"])
 
     # Find closest match to user input
-    closest_match = find_closest_match(movie_title, movie_titles)
+    closest_match = find_closest_match(movie_title, tmdb_data['title'].tolist())
 
     # Check if a close match is found
     if closest_match:
         st.write("Closest match found:", closest_match)
         
+        # Load models
+        collaborative_model = load('movie_recommender_model_collaborative.joblib')
+        content_based_model = load('movie_recommender_model_content-based.joblib')
+        
         # Perform recommendation based on selected filtering method
         if filtering_method == "Collaborative Filtering":
-            collab_filtering_result = collaborative_filtering(closest_match)
+            collab_filtering_result = collaborative_filtering(closest_match, collaborative_model, tmdb_data)
             if collab_filtering_result:
                 st.write("\nTop 10 movies similar to", closest_match, "based on Collaborative Filtering:")
                 for movie_id, similarity_score in collab_filtering_result:
@@ -71,7 +67,7 @@ def main():
             else:
                 st.write("No recommendations found.")
         elif filtering_method == "Content-Based Filtering":
-            content_based_filtering_result = content_based_filtering(closest_match)
+            content_based_filtering_result = content_based_filtering(closest_match, content_based_model, tmdb_data)
             if content_based_filtering_result:
                 st.write("\nTop 10 movies similar to", closest_match, "based on Content-Based Filtering:")
                 for movie_id, similarity_score in content_based_filtering_result:
