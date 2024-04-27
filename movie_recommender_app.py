be82c8f4994cae1c9db0f8084b05137dd833c4bb
Nosna132 <141562783+Nosna132@users.noreply.github.com>
2024-04-28 00:11:37 +0800

    Update movie_recommender_app.py

--- movie_recommender_app.py
+++ movie_recommender_app.py
@@ -6,7 +6,7 @@ from joblib import load
 import difflib
 
 # Set page config to wide mode and dark theme
-st.set_page_config(layout="wide", page_title="Movie Recommender System", page_icon="🎬")
+st.set_page_config(layout="wide", page_title="Movie Recommender System")
 
 # Load data
 tmdb_data = pd.read_csv('tmdb_5000_movies.csv')
@@ -111,17 +111,15 @@ if st.button("Recommend"):
         if filter_choice == "Collaborative Filtering":
             collab_filtering_result = collaborative_filtering(closest_match)
             st.subheader("Top 10 movies similar to {} based on Collaborative Filtering:".format(closest_match))
-            collab_results_df = pd.DataFrame(columns=["Movie", "Similarity Score"])
             for movie in collab_filtering_result:
-                collab_results_df = pd.concat([collab_results_df, pd.DataFrame({"Movie": [tmdb_data.iloc[movie[0]]['title']], "Similarity Score": [movie[1]]})], ignore_index=True)
-            st.table(collab_results_df)
+                st.write("- Movie:", tmdb_data.iloc[movie[0]]['title'])
+                st.write("  Similarity Score:", movie[1])
 
         elif filter_choice == "Content-Based Filtering":
             content_based_filtering_result = content_based_filtering(closest_match)
             st.subheader("Top 10 movies similar to {} based on Content-Based Filtering:".format(closest_match))
-            content_results_df = pd.DataFrame(columns=["Movie", "Similarity Score"])
             for movie in content_based_filtering_result:
-                content_results_df = pd.concat([content_results_df, pd.DataFrame({"Movie": [tmdb_data.iloc[movie[0]]['title']], "Similarity Score": [movie[1]]})], ignore_index=True)
-            st.table(content_results_df)
+                st.write("- Movie:", tmdb_data.iloc[movie[0]]['title'])
+                st.write("  Similarity Score:", movie[1])
     else:
         st.write("There's no movie such as", movie_title, "Please enter another title")
